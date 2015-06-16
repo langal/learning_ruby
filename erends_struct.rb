be932dbfc47f95180a2272f18cbbd5385799004d
EC2 Default User <ec2-user@ip-172-31-33-211.ec2.internal>
2015-06-16 07:33:00 +0000

    nested OpenStruct

---
+++ erends_struct.rb
@@ -0,0 +1,30 @@
+require 'ostruct'
+
+class ErendsStruct
+    def self.make(hash)
+        #struct = OpenStruct.new(hash)
+        object = OpenStruct.new()
+        if (!hash.kind_of?(Hash))
+            raise ArgumentError('Argument is not of type Hash')
+        end
+        hash.keys.each do |key|
+            #puts key + " " + hash[key].class().to_s()
+            if hash[key].respond_to?('keys')
+                object[key] = make(hash[key])
+            elsif hash[key].kind_of?(Array)
+                subArray = Array.new()
+                hash[key].each do |subValue|
+                    if subValue.respond_to?('keys')
+                        subArray << make(subValue)
+                    else
+                        subArray << subValue
+                    end
+                    object[key] = subArray
+                end
+            else
+                object[key] = hash[key]
+            end
+        end
+        return object
+    end
+end
